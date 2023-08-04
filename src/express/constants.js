f97b258dedcdee7d2089427018a76da32541488d
pavel <shirokuiuna@mail.ru>
2023-08-04 09:52:18 +0300

    module3-task2

--- src/express/constants.js
+++ src/express/constants.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 module.exports = {
-  DEFAULT_PORT
+  DEFAULT_PORT,
+  PUBLIC_DIR
 };
--- src/express/express.js
+++ src/express/express.js
@@ -1,14 +1,22 @@
 'use strict';
 
+const chalk = require(`chalk`);
+const path = require(`path`);
 const express = require(`express`);
 const app = express();
-const {DEFAULT_PORT} = require(`./constants`);
+const {DEFAULT_PORT, PUBLIC_DIR} = require(`./constants`);
 const mainRoutes = require(`./routes/main-routes`);
 const registerRoutes = require(`./routes/register-routes`);
 const loginRoutes = require(`./routes/login-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const articlesRoutes = require(`./routes/articles-routes`);
 const searchRoutes = require(`./routes/search-routes`);
+
+
+// Настройка шаблонизатора
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 // Маршрутизация
 app.use(`/`, mainRoutes);
@@ -18,4 +26,13 @@ app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRoutes);
 app.use(`/search`, searchRoutes);
 
-app.listen(DEFAULT_PORT);
+app.listen(DEFAULT_PORT, (err) => {
+  if (err) {
+    console.error(chalk.red(`При создании сервера возникла ошибка`));
+    console.error(err);
+
+    return;
+  }
+
+  console.log(chalk.green(`Сервер фронтенда успешно создан на порту ${DEFAULT_PORT} http://localhost:${DEFAULT_PORT}`));
+});
